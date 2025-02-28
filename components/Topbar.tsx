38c763aff5e678b4a22e78cb8f098437aa04464c
gene21gene <owens.eugene@yahoo.com>
2025-02-27 20:40:54 -0500

    adding heart icon

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,17 +1,20 @@
-'use client';
+"use client";
 
 import React from "react";
+import { Heart } from "lucide-react";
 
 const Topbar = () => {
   return (
-    <div className="font-bebas flex items-center justify-between p-3 bg-[#1E1E1E] text-white text-sm">
-      <div>
+    <div className="bg-[#1E1E1E] text-white text-sm">
+      <div className="max-w-[1440px] mx-auto px-1 flex items-center justify-between p-3">
         <p>🚀 Free Shipping on Orders Over $50!</p>
-      </div>
-      <div className="flex gap-4">
-        <a href="/support" className="hover:text-accent transition">Customer Support</a>
-        <a href="/track-order" className="hover:text-accent transition">Track Your Order</a>
-        <a href="/login" className="hover:text-accent transition">Sign In</a>
+        <div className="flex gap-4 items-center">
+          <a href="/wish-list" className="flex items-center gap-1 hover:text-accent transition">
+            <Heart className="w-4 h-4 text-red-500" />
+            Wishlist
+          </a>
+          <a href="/login" className="hover:text-accent transition">Sign In</a>
+        </div>
       </div>
     </div>
   );
